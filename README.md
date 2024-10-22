c565fa8ee1d238775334bd160f5ceda64f6cb7df
Felipe de Moraes <moraesfd@gmail.com>
2024-10-21 21:58:23 -0300

    Add existing project files to Git

---
+++ README.md
@@ -0,0 +1,29 @@
+# React and TypeScript Fundamentals
+
+This project was created during a course with the goal of learning new features and reviewing the fundamentals of React and TypeScript. It is based on the React + TypeScript template from Vite.
+
+## Technologies Used
+
+- [React](https://reactjs.org/)
+- [TypeScript](https://www.typescriptlang.org/)
+- [Vite](https://vitejs.dev/)
+
+## Installation
+
+To install all project dependencies, run the following command:
+
+```sh
+npm install
+```
+
+## Running the Project
+
+To run the project on port 3001, use the command:
+
+```sh
+npm run dev
+```
+
+## License
+
+This project is licensed under the MIT License.
---
+++ src/components/Comment.tsx
@@ -0,0 +1,57 @@
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
+import styles from "./Comment.module.css";
+import { Avatar } from "./Avatar";
+import { useState } from "react";
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (content: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/moraesfd.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Felipe de Moraes</strong>
+              <time
+                title="26 de Setembro às 23:13h"
+                dateTime="2024-09-26 23:13:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
